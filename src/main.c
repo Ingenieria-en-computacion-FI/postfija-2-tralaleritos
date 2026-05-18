84e07aa4a7d09f63ee64c7ac16aa3cc15c44f42d
Enrique Hernandez <piedraquique@gmail.com>
2026-05-17 22:34:19 -0600

    Main.c completo
    se ha completado main.c

--- src/main.c
+++ src/main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../include/parser.h"
 #include "../include/evaluator.h"
 #include "../include/cola.h"
 
 int main() {
-
     Variable variables[MAX_VARIABLES];
-
-    int totalVariables;
-
+    int totalVariables = 0;
     char expresion[MAX_EXPR];
 
     cargarArchivo(
@@ -22,23 +20,24 @@ int main() {
     printf("Expresion infija:\n");
     printf("%s\n\n", expresion);
 
-    Cola* postfija =
-        infijaAPostfija(expresion);
+    Cola* postfija = infijaAPostfija(expresion);
+
+    if (postfija == NULL) {
+        printf("Error: No se pudo generar la expresion postfija.\n");
+        return 1;
+    }
 
     printf("Expresion postfija:\n");
-
     imprimirCola(postfija);
 
-    float resultado =
-        evaluarPostfija(
-            postfija,
-            variables,
-            totalVariables
-        );
+    float resultado = evaluarPostfija(
+        postfija,
+        variables,
+        totalVariables
+    );
 
     printf("\nResultado:\n");
-    printf("%.2f\n", resultado);
-
+    printf("%.0f\n", resultado)ue; 
     destruirCola(postfija);
 
     return 0;
